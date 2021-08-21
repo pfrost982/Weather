b29df929060b19465cf1bb68daf495ecec0042aa
pfrost982 <power982@gmail.com>
2021-08-21 23:29:33 +0700

    Небольшие переделки модели

--- app/src/main/java/ru/gb/weather/App.kt
+++ app/src/main/java/ru/gb/weather/App.kt
@@ -2,8 +2,11 @@ package ru.gb.weather
 
 import android.app.Application
 import androidx.room.Room
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import ru.gb.weather.model.room.CitiesDao
 import ru.gb.weather.model.room.CitiesDataBase
+import ru.gb.weather.model.web.OpenWeatherWebService
 
 class App : Application() {
 
@@ -16,6 +19,7 @@ class App : Application() {
 
         private var appInstance: App? = null
         private var db: CitiesDataBase? = null
+        private var service: OpenWeatherWebService? = null
 
         fun getCitiesDao(): CitiesDao {
             if (db == null) {
@@ -33,6 +37,22 @@ class App : Application() {
                 }
             }
             return db!!.getDao()
+        }
+
+        fun getServiceOpenWeather(): OpenWeatherWebService {
+            if (service == null) {
+                synchronized(OpenWeatherWebService::class.java) {
+                    if (service == null) {
+                        if (appInstance == null) throw IllegalStateException("Application is null while creating DataBase")
+                        val retrofit: Retrofit = Retrofit.Builder()
+                            .baseUrl("https://api.openweathermap.org/")
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build()
+                        service = retrofit.create(OpenWeatherWebService::class.java)
+                    }
+                }
+            }
+            return service!!
         }
     }
 }
--- app/src/main/java/ru/gb/weather/model/RepositoryImpl.kt
+++ app/src/main/java/ru/gb/weather/model/RepositoryImpl.kt
@@ -1,23 +1,15 @@
 package ru.gb.weather.model
 
 import retrofit2.Callback
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import ru.gb.weather.App
 import ru.gb.weather.model.web.OpenWeatherWebEntity
-import ru.gb.weather.model.web.OpenWeatherWebService
 
 class RepositoryImpl : Repository {
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("https://api.openweathermap.org/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private val service: OpenWeatherWebService = retrofit.create(OpenWeatherWebService::class.java)
-
+    private val serviceOpenWeather = App.getServiceOpenWeather()
     private var citiesDao = App.getCitiesDao()
 
     override fun getWeatherFromServer(city: City, callback: Callback<OpenWeatherWebEntity>) {
-        service.getCurrentWeather(city.cityName).enqueue(callback)
+        serviceOpenWeather.getCurrentWeather(city.cityName).enqueue(callback)
     }
 
     override fun getCitiesListFromLocalStorage(): List<City> =
