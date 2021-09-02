70802268ed9ce9e6658a7a3ff6b835b3fc922879
pfrost982 <power982@gmail.com>
2021-09-02 16:16:04 +0700

    Ключ спрятан, как в GooglMaps

--- app/src/main/java/ru/gb/weather/model/web/OpenWeatherWebService.kt
+++ app/src/main/java/ru/gb/weather/model/web/OpenWeatherWebService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.Query
 import ru.gb.weather.BuildConfig
 
 interface OpenWeatherWebService {
-    @GET("data/2.5/weather?units=metric&lang=ru&APPID=${BuildConfig.WEATHER_API_KEY}")
+    @GET("data/2.5/weather?units=metric&lang=ru&APPID=${BuildConfig.API_OPEN_WEATHER_MAP_ORG_KEY}")
     fun getCurrentWeather(@Query("q") cityName: String): Call<OpenWeatherWebEntity>
 }
